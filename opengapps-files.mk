e460552543b168842ce7385f65c65033a16d2a96
James Muir <james.muir@graphitesoftware.com>
2016-11-28 02:58:48 -0500

    Fix bullhead Marshmallow build
    Copying files to system/vendor isn't allowed when you are building a
    vendor image.  Skip the pittpatt copy statements when the device is
    bullhead (since we build a vendor image).
    
    Rather than checking the device here, what we should really be doing
    is skipping those copy statements for any time a vendor image is being
    built.  Inside the "build" project, the statement "ifdef
    BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE" is used to do that, but that
    doesn't work here because of the way the opengapps makefiles is
    included (they are processed before
    device/lge/bullhead/BoardConfig.mk).

--- opengapps-files.mk
+++ opengapps-files.mk
@@ -6,7 +6,11 @@ PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,framework)
 ifneq ($(filter $(TARGET_GAPPS_VARIANT),pico),)
 # vendor/pittpatt seems to be removed on N+ (so only copy it to older than N)
 ifeq ($(filter $(call get-allowed-api-levels),24),)
+ifneq ($(filter bullhead, $(TARGET_DEVICE)),)
+# Graphite change: for bullhead, we build a vendor image, and so
+# copying things to system/vendor isn't allowed.
   PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,vendor/pittpatt)
+endif
 endif
   PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,usr/srec)
 endif
