e42fb22599454e8c30ade0f09134f2ed6a505757
James Muir <james.muir@graphitesoftware.com>
2018-05-04 15:52:13 -0400

    Drop com.google.android.camera.experimental2017 from gsi
    com.google.android.camera.experimental2017.jar breaks Google Camera
    when using the gsi on the Pixel.  Drop the jar and xml when building a
    gsi.

--- opengapps-files.mk
+++ opengapps-files.mk
@@ -1,5 +1,10 @@
-# Always needed
-PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,framework)
+gapps_framework_files := $(call gapps-copy-to-system,all,framework)
+
+ifneq ($(filter gsi_%,$(TARGET_PRODUCT)),)
+  gapps_framework_files := $(filter-out %com.google.android.camera.experimental2017.jar,$(gapps_framework_files))
+endif
+
+PRODUCT_COPY_FILES += $(gapps_framework_files)
 
 GAPPS_NEXUS_CODENAMES += \
     full_maguro \
@@ -25,6 +30,10 @@ GAPPS_PIXEL_CODENAMES += \
     aosp_wahoo
 
 gapps_etc_files := $(call gapps-copy-to-system,all,etc)
+
+ifneq ($(filter gsi_%,$(TARGET_PRODUCT)),)
+  gapps_etc_files := $(filter-out %com.google.android.camera.experimental2017.xml,$(gapps_etc_files))
+endif
 
 # Remove google_build.xml on non-Pixel devices
 ifeq ($(filter $(GAPPS_PIXEL_CODENAMES),$(TARGET_PRODUCT)),)
