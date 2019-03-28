c95a042637d33e78875390081a37fd4edc2b209e
James Muir <james.muir@graphitesoftware.com>
2019-03-28 11:57:17 -0400

    Revert "G0-714 fix Google Camera"
    This reverts commit cf3923bc4f38d4e41cc75d68ef83998a0091c99c.
    
    This is an oreo specific fix.  R5.5-master is used to build nougat
    images only.

--- modules/GoogleCamera/Android.mk
+++ modules/GoogleCamera/Android.mk
@@ -3,8 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := GoogleCamera
 LOCAL_PACKAGE_NAME := com.google.android.googlecamera
-# on oreo, the api-level 26 apk does not work.  Use 25 instead.
-GAPPS_LOCAL_SRC_FILES_OVERRIDE := vendor/opengapps/sources/arm64/app/com.google.android.googlecamera/25/nodpi/41174009.apk
 
 GAPPS_LOCAL_OVERRIDES_MIN_VARIANT :=
 GAPPS_LOCAL_OVERRIDES_PACKAGES := Camera Camera2 MotCamera Snap
