f44c05c09d5765e2ae074b3d347b0eb6ae74fdde
James Muir <james.muir@graphitesoftware.com>
2019-03-28 12:01:11 -0400

    Fix nougat build
    in-build dex-optimization was crashing for these two apks.

--- modules/CloudPrint2/Android.mk
+++ modules/CloudPrint2/Android.mk
@@ -3,5 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := CloudPrint2
 LOCAL_PACKAGE_NAME := com.google.android.apps.cloudprint
+LOCAL_DEX_PREOPT := false
 
 include $(BUILD_GAPPS_PREBUILT_APK)
